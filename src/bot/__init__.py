40ab54d59fed9b5354562b06ecdbe27b77e690e1
Maxim Zharinoff <m.zharinoff@vk.com>
2025-07-31 18:43:39 +0700

    modify __init__.py to bot module: import and initialize bot, dispatcher, logger

---
+++ src/bot/__init__.py
@@ -0,0 +1,16 @@
+import logging
+import os
+from aiogram import Bot, Dispatcher
+from dotenv import load_dotenv
+
+load_dotenv()
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+bot = Bot(token=os.getenv("TG_BOT_TOKEN"))
+dp = Dispatcher()
+
+from . import commands
+
+__all__ = ["bot", "dp", "commands"]
